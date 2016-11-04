678cbcd86964189a757d221ddca067fa4311a6b9
Jannot <denis.jannot@emc.com>
2016-11-04 13:32:41 +0100

    Adding Object User Authentication support

--- ecspics.go
+++ ecspics.go
@@ -158,6 +158,7 @@ type UserSecretKeyResult struct {
 }
 
 // Validate credentials using the ECS self service API
+/*
 func Login(w http.ResponseWriter, r *http.Request) {
   if r.Method == "POST" {
     r.ParseForm()
@@ -213,6 +214,99 @@ func Login(w http.ResponseWriter, r *http.Request) {
       }
       session.Values["AccessKey"] = user
       session.Values["SecretKey"] = secretKey
+      err = sessions.Save(r, w)
+      if err != nil {
+        rendering.HTML(w, http.StatusInternalServerError, "error", http.StatusInternalServerError)
+      }
+      http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+    }
+  } else {
+    rendering.HTML(w, http.StatusOK, "login", nil)
+  }
+}
+*/
+
+// Login using an AD or object user
+func Login(w http.ResponseWriter, r *http.Request) {
+  // If informaton received from the form
+  if r.Method == "POST" {
+    session, err := store.Get(r, "session-name")
+    if err != nil {
+      rendering.HTML(w, http.StatusInternalServerError, "error", http.StatusInternalServerError)
+    }
+    r.ParseForm()
+    authentication := r.FormValue("authentication")
+    user := r.FormValue("user")
+    password := r.FormValue("password")
+    endpoint := r.FormValue("endpoint")
+    // For AD authentication, needs to retrieve the S3 secret key from ECS using the ECS management API
+    if authentication == "ad" {
+      url, err := url.Parse(endpoint)
+      if err != nil{
+          rendering.HTML(w, http.StatusOK, "login", "Check the endpoint")
+      }
+      hostname := url.Host
+      if strings.Contains(hostname, ":") {
+        hostname = strings.Split(hostname, ":")[0]
+      }
+      tr := &http.Transport{
+        TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+      }
+      client := &http.Client{Transport: tr}
+      // Get an authentication token from ECS
+      req, _ := http.NewRequest("GET", "https://" + hostname + ":4443/login", nil)
+      req.SetBasicAuth(user, password)
+      resp, err := client.Do(req)
+      if err != nil{
+          log.Print(err)
+      }
+      if resp.StatusCode == 401 {
+        rendering.HTML(w, http.StatusOK, "login", "Check your crententials and that you're allowed to generate a secret key on ECS")
+      } else {
+        // Get the secret key from ECS
+        req, _ = http.NewRequest("GET", "https://" + hostname + ":4443/object/secret-keys", nil)
+        headers := map[string][]string{}
+        headers["X-Sds-Auth-Token"] = []string{resp.Header.Get("X-Sds-Auth-Token")}
+        req.Header = headers
+        resp, err = client.Do(req)
+        if err != nil{
+            log.Print(err)
+        }
+        buf := new(bytes.Buffer)
+        buf.ReadFrom(resp.Body)
+        secretKey := ""
+        userSecretKeysResult := &UserSecretKeysResult{}
+        xml.NewDecoder(buf).Decode(userSecretKeysResult)
+        secretKey = userSecretKeysResult.SecretKey1
+        // If a secret key doesn't exist yet for this object user, needs to generate it
+        if secretKey == "" {
+          req, _ = http.NewRequest("POST", "https://" + hostname + ":4443/object/secret-keys", bytes.NewBufferString("<secret_key_create_param></secret_key_create_param>"))
+          headers["Content-Type"] = []string{"application/xml"}
+          req.Header = headers
+          resp, err = client.Do(req)
+          if err != nil{
+              log.Print(err)
+          }
+          buf = new(bytes.Buffer)
+          buf.ReadFrom(resp.Body)
+          userSecretKeyResult := &UserSecretKeyResult{}
+          xml.NewDecoder(buf).Decode(userSecretKeyResult)
+          secretKey = userSecretKeyResult.SecretKey
+        }
+        session.Values["AccessKey"] = user
+        session.Values["SecretKey"] = secretKey
+        session.Values["Endpoint"] = endpoint
+        err = sessions.Save(r, w)
+        if err != nil {
+          rendering.HTML(w, http.StatusInternalServerError, "error", http.StatusInternalServerError)
+        }
+        http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+      }
+    // For an object user authentication, use the credentials as-is
+    } else {
+      session.Values["AccessKey"] = user
+      session.Values["SecretKey"] = password
+      session.Values["Endpoint"] = endpoint
       err = sessions.Save(r, w)
       if err != nil {
         rendering.HTML(w, http.StatusInternalServerError, "error", http.StatusInternalServerError)
